4175a81ac5b56ce1a9b501408cad188eadf0a212
Sam <samfertap@gmail.com>
2025-08-17 02:59:23 -0600

    actualización de rama

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,7 @@ import Escritorio from './pages/Escritorio.jsx'
 import Forms from './pages/forms.jsx'
 
 
+
 function App() {
   return (
     <Routes>
@@ -17,6 +18,7 @@ function App() {
       <Route path="/escritorio" element={<Escritorio />} />
       <Route path="/forms" element={<Forms />} />
       {/* Aquí puedes agregar más rutas según vayas creando más páginas */}
+      
     </Routes>
   )
 }
